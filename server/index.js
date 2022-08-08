56242f7d8b3d47c8990e861115f9c7a1e2f03402
alaqsaka <aqsaabidin15@gmail.com>
2022-08-08 16:58:27 +0700

    create routes and controllers

--- server/index.js
+++ server/index.js
@@ -1,6 +1,10 @@
 import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
+import userRoutes from "./routes/users.js";
+import videoRoutes from "./routes/videos.js";
+import commentRoutes from "./routes/comments.js";
+import authRoutes from "./routes/auth.js";
 
 const app = express();
 dotenv.config();
@@ -9,11 +13,18 @@ const connect = () => {
   console.log("Connecting to DB ... ");
   mongoose
     .connect(process.env.MONGO)
-    .then(() => console.log("Connected to DB!"))
+    .then(() => {
+      console.log("Connected to DB");
+    })
     .catch((err) => {
       throw err;
     });
 };
+
+app.use("/api/v1/users", userRoutes);
+app.use("/api/v1/videos", videoRoutes);
+app.use("/api/v1/comments", commentRoutes);
+app.use("/api/v1/auth", authRoutes);
 
 app.listen(8800, () => {
   connect();
