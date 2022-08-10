2865a655afd9357682c0705c036f7213b34e2918
alaqsaka <aqsaabidin15@gmail.com>
2022-08-10 17:08:42 +0700

    create controllers for getuser, update user, delete user

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,6 +1,35 @@
 import express from "express";
-import { test } from "../controllers/user.js";
+import {
+  deleteUser,
+  dislike,
+  getUser,
+  like,
+  subscribe,
+  update,
+} from "../controllers/user.js";
+import { verifyToken } from "../verifyToken.js";
 
 const router = express.Router();
+
+// update user
+router.put("/:id", verifyToken, update);
+
+// delete user
+router.delete("/:id", verifyToken, deleteUser);
+
+// get a user
+router.get("/find/:id", getUser);
+
+// subscribe a user
+router.put("/sub/:id", verifyToken, subscribe);
+
+// unsubscribe a user
+router.put("/unsub/:id", verifyToken, update);
+
+// like a video
+router.put("/like/:videoId", verifyToken, like);
+
+// dislike a video
+router.put("/dislike/:videoId", verifyToken, dislike);
 
 export default router;
